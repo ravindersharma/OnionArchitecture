1a87f4fca3e79982973251667b0fdda10027bbee
ravindersharma583 <ravindersharma583@gmail.com>
2022-07-31 21:06:03 +0530

    DbContext Implemented

---
+++ Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context
+{
+    public class ApplicationDbContext : DbContext, IApplicaitonDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get ; set ; }
+
+        public async Task<int> SaveChangesAsync()
+        {
+           return await base.SaveChangesAsync();
+        }
+    }
+}
