a36cfcf5faa250e22c9df28b9ca696ad64e4b6ed
ravindersharma583 <ravindersharma583@gmail.com>
2022-07-31 17:18:38 +0530

    Updated Webapi Swagger docuemetation and code clean up

---
+++ WebApi/HostingExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Application;
+using Microsoft.OpenApi.Models;
+
+namespace WebApi
+{
+    public static class HostingExtensions
+    {
+
+        /// <summary>
+        /// Method to configure services 
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c => {
+                c.IncludeXmlComments($"{System.AppDomain.CurrentDomain.BaseDirectory}\\OnionArchitecture.xml");
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "OnionArchitecture"
+                });
+            });
+
+            // Add applicaition layer here
+            builder.Services.AddApplication();
+
+
+            return builder.Build();
+        }
+
+        /// <summary>
+        /// Method to configure middleware (pipeline) for api
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            //confiure middlerware for app
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
+
+           
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+
+        }
+
+
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,34 +1,10 @@
 using Microsoft.OpenApi.Models;
+using WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => {
-    c.IncludeXmlComments($"{System.AppDomain.CurrentDomain.BaseDirectory}\\OnionArchitecture.xml");
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "OnionArchitecture"
-    });
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+//var app = builder.Build();
+var app = builder.ConfigureServices()
+                 .ConfigurePipeline();
 
 app.Run();
